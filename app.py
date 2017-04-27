3073f90ca6c81d33c919b723d04794dd5d0af55e
jamescoxon <jacoxon@ubuntu-512mb-lon1-01.localdomain>
2017-04-27 19:16:21 +0000

    added python filesy

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask
+from flask import render_template, redirect, url_for, request, abort, flash, session
+
+app = Flask(__name__)
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import json
+import dataset
+
+pool_address = 'xrb_317ngidjcgamhiq79kztd69kx1kiusybhzakya9xhxjoaabkhrmp8mr4wio4'
+
+# connecting to a SQLite database
+db = dataset.connect('sqlite:///mydatabase.db')
+
+# get a reference to the table 'user'
+table = db['user']
+
+def payout_data():
+
+        url = 'https://raiblockscommunity.net/faucet/paylist.php' # Set destination URL here
+        data = {'acc': pool_address, 'json': 1}
+        request = Request(url, urlencode(data).encode())
+        json = urlopen(request).read().decode()
+        return json
+
+@app.route('/')
+def start():
+    return render_template('info.html')
+
+@app.route('/pool')
+def default_pool():
+	return render_template('info.html')
+
+@app.route('/pool/<address>')
+def pool(address=None):
+	total_address_claims = table.find(address=address)
+	unsubmitted_claims = 0
+	unpaid_claims = 0
+	for xrb_address in total_address_claims:
+#		print("XRB:", xrb_address['address'], " sub:", xrb_address['submitted'])
+		if xrb_address['submitted'] == 0:
+			unsubmitted_claims = unsubmitted_claims + 1
+		if xrb_address['paid'] == 0:
+			unpaid_claims = unpaid_claims + 1
+#	print(unsubmitted_claims)
+
+	all_unpaid = table.find(paid=0)
+	unpaid_address = []
+	for unpaid in all_unpaid:
+        #       print(unpaid['address'])
+		unpaid_address.append(unpaid['address'])
+
+	len_unpaid_address = len(unpaid_address)
+	if len_unpaid_address > 0:
+		perc_claims = (int(unpaid_address.count(address)) / len_unpaid_address) * 100
+	else:
+		perc_claims = 0
+	print(perc_claims)
+
+	return render_template('test.html', address=address, unsub_claims=unsubmitted_claims, unpai_claims=unpaid_claims, perc_claims=perc_claims, total_claims=len_unpaid_address)
+
+@app.route('/error')
+def error():
+        return render_template('error.html')
+
+@app.route("/submit", methods=["POST"])
+def submit():
+	response = request.form.get('g-recaptcha-response')
+	print(response)
+	complete_address = request.referrer
+	current_address = complete_address.split("/")
+	xrb_address = current_address[-1]
+	# Insert a new record.
+	table.insert(dict(address=xrb_address, paid=0, grecaptcha=response, submitted=0))
+	if len(response) > 0:
+		return redirect(request.referrer)
+	else:
+		return redirect(url_for('error'))
+
+@app.route('/stats')
+def display_stats():
+	payout_raw = payout_data()
+	payout = json.loads(payout_raw)
+	#print(payout_raw)
+
+	acc_payout = payout['pending']
+
+	print("Pending: ", payout['pending'])
+	print("Validated claims: ", acc_payout[0]['pending'])
+	print("Threshold: ", payout['threshold'])
+
+	all_unpaid = table.find(paid=0)
+	unpaid_address = []
+	for unpaid in all_unpaid:
+	#       print(unpaid['address'])
+		unpaid_address.append(unpaid['address'])
+
+	list_unpaid_address = set(unpaid_address)
+	len_unpaid_address = len(unpaid_address)
+	for unique_address in list_unpaid_address:
+		perc_claims = (int(unpaid_address.count(unique_address)) / len_unpaid_address) * 100
+		print(unique_address, " : ", unpaid_address.count(unique_address), " " ,perc_claims, "%")
+
+	return render_template('stats.html', len_unpaid=len_unpaid_address, payout_threshold=payout['threshold'] )
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ submit.py
@@ -0,0 +1,123 @@
+
+import dataset
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import json
+import time
+
+pool_address = 'xrb_317ngidjcgamhiq79kztd69kx1kiusybhzakya9xhxjoaabkhrmp8mr4wio4'
+top_60 = 0
+
+def post_data(data):
+
+	url = 'https://raiblockscommunity.net/faucet/elaborate.php' # Set destination URL here
+
+	request = Request(url, urlencode(data).encode())
+	json = urlopen(request).read().decode()
+	return str(json)
+
+def payout_data():
+
+	url = 'https://raiblockscommunity.net/faucet/paylist.php' # Set destination URL here
+	data = {'acc': pool_address, 'json': 1}
+	request = Request(url, urlencode(data).encode())
+	json = urlopen(request).read().decode()
+	return json
+
+# connecting to a SQLite database
+db = dataset.connect('sqlite:///mydatabase.db')
+
+# get a reference to the table 'user'
+table = db['user']
+while 1:
+	total_address_claims = table.find(submitted=0)
+	cap_data = []
+	for xrb_address in total_address_claims:
+		print("XRB:", xrb_address['address'], " sub:", xrb_address['submitted'], " paid:", xrb_address['paid'])
+		cap_data.append(xrb_address['grecaptcha'])
+
+	if len(cap_data) > 0:
+		json_string = json.dumps(cap_data)
+		data = {}
+		data['ask_address']=pool_address
+		data['donate']=1
+		data['accepted']=1
+		data['captchas']=json_string
+
+#	print(data)
+
+		data_response = post_data(data)
+		print(data_response)
+
+		split_data_response = data_response.split('{')
+		print("{", split_data_response[1])
+		first_data_response = "{" + split_data_response[1]
+		json_response = json.loads(first_data_response)
+		print(json_response)
+
+		#{"error":"no","valid":1,"thanks":1,"valid_captchas":[0]}
+		if json_response['error'] == "no":
+			print("Submit accepted, valid: ", json_response['valid'])
+			for update_data in cap_data:
+				#print(update_data)
+				table.update(dict(grecaptcha=update_data, submitted=1), ['grecaptcha'])
+			print("Database updated")
+		else:
+			for update_data in cap_data:
+				table.delete(grecaptcha=update_data)
+			print("Database updated")
+
+
+	payout_raw = payout_data()
+	payout = json.loads(payout_raw)
+	print(payout_raw)
+
+	acc_payout = payout['pending']
+	exp_payout = acc_payout[0]['expected-pay']
+	print("Expected: ", exp_payout)
+
+	if ((top_60 == 1) and int(exp_payout) == 0):
+		payout_id = time.time()
+		print("Now update database:", payout_id)
+		top_60 = 0
+		all_unpaid = table.find(paid=0)
+		for unpaid in all_unpaid:
+			data = dict(id=unpaid['id'], paid=payout_id)
+			table.update(data, ['id'])
+		print("Table updated")
+
+	if (int(exp_payout) > 0):
+		print("In the top 60")
+		top_60 = 1
+
+		if (int(acc_payout[0]['pending']) == 0):
+			payout_id = time.time()
+			print("Now update database:", payout_id)
+#	all_unpaid = table.find(paid=0)
+#	for unpaid in all_unpaid:
+#		data = dict(id=unpaid['id'], paid=payout_id)
+#		table.update(data, ['id'])
+#	print("Table updated")
+	else:
+		print("Continue claiming")
+
+	print("Pending: ", payout['pending'])
+	print("Validated claims: ", acc_payout[0]['pending'])
+	print("Threshold: ", payout['threshold'])
+
+	all_unpaid = table.find(paid=0)
+	unpaid_address = []
+	for unpaid in all_unpaid:
+#	print(unpaid['address'])
+		unpaid_address.append(unpaid['address'])
+
+	total_unpaid_address = len(unpaid_address)
+
+	list_unpaid_address = set(unpaid_address)
+	for unique_address in list_unpaid_address:
+		perc_claims = (int(unpaid_address.count(unique_address)) / total_unpaid_address) * 100
+		print(unique_address, " : ", unpaid_address.count(unique_address), " " ,perc_claims, "%")
+
+	print("Done")
+
+	time.sleep(90)

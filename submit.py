ead15835b2a5dad855d4c7faebfbffbe09dc2be1
jamescoxon <jacoxon@ubuntu-512mb-lon1-01.localdomain>
2017-04-28 05:59:47 +0000

    added code to stop submit.py script crashing if there is an error on the main server. Also removed some prints as not needed.

--- submit.py
+++ submit.py
@@ -2,6 +2,7 @@
 import dataset
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 import json
 import time
 
@@ -12,17 +13,34 @@ def post_data(data):
 
 	url = 'https://raiblockscommunity.net/faucet/elaborate.php' # Set destination URL here
 
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
-	return str(json)
+	try:
+		request = Request(url, urlencode(data).encode())
+		json = urlopen(request).read().decode()
+		return str(json)
+
+	except HTTPError as e:
+		print(e)
+		return "error"
+	except URLError as e:
+		print(e)
+	return "error"
+
 
 def payout_data():
 
 	url = 'https://raiblockscommunity.net/faucet/paylist.php' # Set destination URL here
-	data = {'acc': pool_address, 'json': 1}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
-	return json
+	try:
+		data = {'acc': pool_address, 'json': 1}
+		request = Request(url, urlencode(data).encode())
+		json = urlopen(request).read().decode()
+		return json
+
+	except HTTPError as e:
+		print(e)
+		return "error"
+	except URLError as e:
+		print(e)
+		return "error"
 
 # connecting to a SQLite database
 db = dataset.connect('sqlite:///mydatabase.db')
@@ -50,87 +68,93 @@ while 1:
 #	print(data)
 
 		data_response = post_data(data)
-		print(data_response)
+		if data_response == "error":
+			print ("Error with connection, skip processing")
+		else:
+			print(data_response)
 
-		split_data_response = data_response.split('{')
-		print("{", split_data_response[1])
-		first_data_response = "{" + split_data_response[1]
-		json_response = json.loads(first_data_response)
-		print(json_response)
+			split_data_response = data_response.split('{')
+			#print("{", split_data_response[1])
+			first_data_response = "{" + split_data_response[1]
+			json_response = json.loads(first_data_response)
+			#print(json_response)
 
 		#{"error":"no","valid":1,"thanks":1,"valid_captchas":[0]}
-		if json_response['error'] == "no":
-			print("Submit accepted, valid: ", json_response['valid'])
-			diff = total_claims_data - int(json_response['valid'])
-			print("Diff: ", diff)
-			print("Array of submissions: ", json_response['valid_captchas'])
+			if json_response['error'] == "no":
+				print("Submit accepted, valid: ", json_response['valid'])
+				diff = total_claims_data - int(json_response['valid'])
+				print("Diff: ", diff)
+				print("Array of submissions: ", json_response['valid_captchas'])
 
-			for submissions in json_response['valid_captchas']:
-				table.update(dict(grecaptcha=cap_data[submissions], submitted=1), ['grecaptcha'])
+				for submissions in json_response['valid_captchas']:
+					table.update(dict(grecaptcha=cap_data[submissions], submitted=1), ['grecaptcha'])
 
-			if diff > 0:
-				#Remove invalid claims
-				claims_left = table.find(submitted=0)
-				for claims in claims_left:
-					print(claims['id'])
-					table.delete(id=claims['id'])
+				if diff > 0:
+					#Remove invalid claims
+					claims_left = table.find(submitted=0)
+					for claims in claims_left:
+						print(claims['id'])
+						table.delete(id=claims['id'])
 
-			print("Database updated")
-		else:
-			for update_data in cap_data:
-				table.delete(grecaptcha=update_data)
-			print("Database updated")
+				print("Database updated")
+			else:
+				for update_data in cap_data:
+					table.delete(grecaptcha=update_data)
+				print("Database updated")
 
 
 	payout_raw = payout_data()
-	payout = json.loads(payout_raw)
-	print(payout_raw)
+	if payout_raw == "error":
+		print ("Error with connection, skip processing")
+	else:
+		payout = json.loads(payout_raw)
+		print(payout_raw)
 
-	acc_payout = payout['pending']
-	exp_payout = acc_payout[0]['expected-pay']
-	print("Expected: ", exp_payout)
+		acc_payout = payout['pending']
+		exp_payout = acc_payout[0]['expected-pay']
+		print("Expected: ", exp_payout)
 
-	if ((top_60 == 1) and int(exp_payout) == 0):
-		payout_id = time.time()
-		print("Now update database:", payout_id)
-		top_60 = 0
-		all_unpaid = table.find(paid=0)
-		for unpaid in all_unpaid:
-			data = dict(id=unpaid['id'], paid=payout_id)
-			table.update(data, ['id'])
-		print("Table updated")
-
-	if (int(exp_payout) > 0):
-		print("In the top 60")
-		top_60 = 1
-
-		if (int(acc_payout[0]['pending']) == 0):
+		if ((top_60 == 1) and int(exp_payout) == 0):
 			payout_id = time.time()
 			print("Now update database:", payout_id)
+			top_60 = 0
+			all_unpaid = table.find(paid=0)
+			for unpaid in all_unpaid:
+				data = dict(id=unpaid['id'], paid=payout_id)
+				table.update(data, ['id'])
+			print("Table updated")
+
+		if (int(exp_payout) > 0):
+			print("In the top 60")
+			top_60 = 1
+
+			if (int(acc_payout[0]['pending']) == 0):
+				payout_id = time.time()
+				print("Now update database:", payout_id)
 #	all_unpaid = table.find(paid=0)
 #	for unpaid in all_unpaid:
 #		data = dict(id=unpaid['id'], paid=payout_id)
 #		table.update(data, ['id'])
 #	print("Table updated")
-	else:
-		print("Continue claiming")
+		else:
+			print("Continue claiming")
 
-	print("Pending: ", payout['pending'])
-	print("Validated claims: ", acc_payout[0]['pending'])
-	print("Threshold: ", payout['threshold'])
+		print("Pending: ", payout['pending'])
+		print("Validated claims: ", acc_payout[0]['pending'])
+		print("Threshold: ", payout['threshold'])
 
-	all_unpaid = table.find(paid=0)
-	unpaid_address = []
-	for unpaid in all_unpaid:
+		all_unpaid = table.find(paid=0)
+		unpaid_address = []
+		for unpaid in all_unpaid:
 #	print(unpaid['address'])
-		unpaid_address.append(unpaid['address'])
+			unpaid_address.append(unpaid['address'])
 
-	total_unpaid_address = len(unpaid_address)
+		total_unpaid_address = len(unpaid_address)
 
-	list_unpaid_address = set(unpaid_address)
-	for unique_address in list_unpaid_address:
-		perc_claims = (int(unpaid_address.count(unique_address)) / total_unpaid_address) * 100
-		print(unique_address, " : ", unpaid_address.count(unique_address), " " ,perc_claims, "%")
+		list_unpaid_address = set(unpaid_address)
+		for unique_address in list_unpaid_address:
+			perc_claims = (int(unpaid_address.count(unique_address)) / total_unpaid_address) * 100
+			print(unique_address, " : ", unpaid_address.count(unique_address), " " ,perc_claims, "%")
 
 	print("Done")
 
